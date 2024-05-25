9d746caf575bc837c8a684918bcc6c6e854df949
GabsGullo <gabrielgullo30@gmail.com>
2024-05-24 21:57:51 -0300

    add spring

---
+++ src/main/java/com/es2/vadebicicleta/es2/vadebicicleta/aluguel/Application.java
@@ -0,0 +1,13 @@
+package com.es2.vadebicicleta.es2.vadebicicleta.aluguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/es2/vadebicicleta/es2/vadebicicleta/aluguel/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es2.vadebicicleta.es2.vadebicicleta.aluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
