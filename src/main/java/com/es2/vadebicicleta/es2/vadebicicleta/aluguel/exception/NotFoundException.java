258e4243cca7b8708648272c2fbdfbe2616bc3bb
GabsGullo <gabrielgullo30@gmail.com>
2024-05-27 01:48:23 -0300

    fixing minor issue

--- src/main/java/com/es2/vadebicicleta/es2/vadebicicleta/aluguel/exception/NotFoundException.java
+++ src/main/java/com/es2/vadebicicleta/es2/vadebicicleta/aluguel/exception/NotFoundException.java
@@ -3,15 +3,10 @@ package com.es2.vadebicicleta.es2.vadebicicleta.aluguel.exception;
 
 public class NotFoundException extends RuntimeException{
 
-    private String mensagem;
-    private String codigo;
-
     public NotFoundException() {}
 
     public NotFoundException(String mensagem, String codigo) {
         super(mensagem);
-        this.mensagem = mensagem;
-        this.codigo = codigo;
     }
 
 
--- src/main/java/com/es2/vadebicicleta/es2/vadebicicleta/aluguel/exception/UnprocessableEntityException.java
+++ src/main/java/com/es2/vadebicicleta/es2/vadebicicleta/aluguel/exception/UnprocessableEntityException.java
@@ -2,12 +2,7 @@ package com.es2.vadebicicleta.es2.vadebicicleta.aluguel.exception;
 
 public class UnprocessableEntityException extends RuntimeException{
 
-    private String mensagem;
-    private String codigo;
-
     public UnprocessableEntityException(String mensagem, String codigo) {
         super(mensagem);
-        this.mensagem = mensagem;
-        this.codigo = codigo;
     }
 }
