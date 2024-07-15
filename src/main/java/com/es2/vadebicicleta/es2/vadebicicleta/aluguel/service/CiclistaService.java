ea54fd70e0e3aec999076486bf86f2b84b98ad55
GabsGullo <gabrielgullo30@gmail.com>
2024-07-15 01:20:49 -0300

    fixing maintainability issues

--- src/main/java/com/es2/vadebicicleta/es2/vadebicicleta/aluguel/service/CiclistaService.java
+++ src/main/java/com/es2/vadebicicleta/es2/vadebicicleta/aluguel/service/CiclistaService.java
@@ -66,56 +66,89 @@ public class CiclistaService {
         return repository.save(ciclistaDesativado);
     }
 
-    private void validateCiclista(Ciclista ciclista){
+    private void validateCiclista(Ciclista ciclista) {
         String objectName = "ciclista";
         BindingResult result = new BeanPropertyBindingResult(ciclista, objectName);
 
-        if (ciclista.getStatus() == null) {
-            result.addError(new FieldError(objectName, "status", "Status não pode ser nulo"));
-        }
-        if (ciclista.getNome() == null || ciclista.getNome().isEmpty()) {
-            result.addError(new FieldError(objectName, "nome", "Nome não pode ser nulo ou vazio"));
-        }
-        if (ciclista.getNascimento() == null || ciclista.getNascimento().isEmpty()) {
-            result.addError(new FieldError(objectName, "nascimento", "Nascimento não pode ser nulo ou vazio"));
-        }
-        if (ciclista.getNacionalidade() == null) {
-            result.addError(new FieldError(objectName, "nacionalidade", "Nacionalidade não pode ser nula"));
-        }
-        if (ciclista.getEmail() == null || !isValidEmail(ciclista.getEmail())) {
-            result.addError(new FieldError(objectName, "email", "Email inválido"));
-        }
-        if (ciclista.getSenha() == null || ciclista.getSenha().isEmpty()) {
-            result.addError(new FieldError(objectName, "senha", "Senha não pode ser nula ou vazia"));
-        }
-
-        boolean temUmDocumentoValido = true;
-        if(ciclista.getCpf() == null && ciclista.getPassaporte() == null){
-            result.addError(new FieldError(objectName, "cpf", "Nenhum documento informado"));
-            result.addError(new FieldError(objectName, "passaporte", "Nenhum documento informado"));
-            temUmDocumentoValido = false;
-        }
-        if(ciclista.getCpf() != null && ciclista.getPassaporte() != null){
-            result.addError(new FieldError(objectName, "cpf", "CPF e passaporte informados"));
-            result.addError(new FieldError(objectName, "passaporte", "CPF e passaporte informados"));
-        }
-        if (ciclista.getCpf() != null && !isValidCPF(ciclista.getCpf())) {
-            result.addError(new FieldError(objectName, "cpf", "CPF inválido"));
-        }
-        if (ciclista.getPassaporte() != null) {
-            if (ciclista.getPassaporte().getNumero() == null || ciclista.getPassaporte().getNumero().isEmpty()) {
-                result.addError(new FieldError(objectName, "passaporte.numero", "Número do passaporte não pode ser nulo ou vazio"));
-            }
-            if (ciclista.getPassaporte().getValidade() == null || ciclista.getPassaporte().getValidade().isEmpty()) {
-                result.addError(new FieldError(objectName, "passaporte.validade", "Validade do passaporte não pode ser nula ou vazia"));
-            }
-            if (ciclista.getPassaporte().getPais() == null || ciclista.getPassaporte().getPais().isEmpty()) {
-                result.addError(new FieldError(objectName, "passaporte.pais", "País do passaporte não pode ser nulo ou vazio"));
-            }
-        }
+        validateStatus(ciclista, result);
+        validateNome(ciclista, result);
+        validateNascimento(ciclista, result);
+        validateNacionalidade(ciclista, result);
+        validateEmail(ciclista, result);
+        validateSenha(ciclista, result);
+        validateDocumentos(ciclista, result);
 
         if (result.hasErrors()) {
             throw new ValidacaoException(result);
+        }
+    }
+
+    private void validateStatus(Ciclista ciclista, BindingResult result) {
+        if (ciclista.getStatus() == null) {
+            result.addError(new FieldError("ciclista", "status", "Status não pode ser nulo"));
+        }
+    }
+
+    private void validateNome(Ciclista ciclista, BindingResult result) {
+        if (ciclista.getNome() == null || ciclista.getNome().isEmpty()) {
+            result.addError(new FieldError("ciclista", "nome", "Nome não pode ser nulo ou vazio"));
+        }
+    }
+
+    private void validateNascimento(Ciclista ciclista, BindingResult result) {
+        if (ciclista.getNascimento() == null || ciclista.getNascimento().isEmpty()) {
+            result.addError(new FieldError("ciclista", "nascimento", "Nascimento não pode ser nulo ou vazio"));
+        }
+    }
+
+    private void validateNacionalidade(Ciclista ciclista, BindingResult result) {
+        if (ciclista.getNacionalidade() == null) {
+            result.addError(new FieldError("ciclista", "nacionalidade", "Nacionalidade não pode ser nula"));
+        }
+    }
+
+    private void validateEmail(Ciclista ciclista, BindingResult result) {
+        if (ciclista.getEmail() == null || !isValidEmail(ciclista.getEmail())) {
+            result.addError(new FieldError("ciclista", "email", "Email inválido"));
+        }
+    }
+
+    private void validateSenha(Ciclista ciclista, BindingResult result) {
+        if (ciclista.getSenha() == null || ciclista.getSenha().isEmpty()) {
+            result.addError(new FieldError("ciclista", "senha", "Senha não pode ser nula ou vazia"));
+        }
+    }
+
+    private void validateDocumentos(Ciclista ciclista, BindingResult result) {
+        if (ciclista.getCpf() == null && ciclista.getPassaporte() == null) {
+            result.addError(new FieldError("ciclista", "cpf", "Nenhum documento informado"));
+            result.addError(new FieldError("ciclista", "passaporte", "Nenhum documento informado"));
+        } else if (ciclista.getCpf() != null && ciclista.getPassaporte() != null) {
+            result.addError(new FieldError("ciclista", "cpf", "CPF e passaporte informados"));
+            result.addError(new FieldError("ciclista", "passaporte", "CPF e passaporte informados"));
+        } else {
+            validateCpf(ciclista, result);
+            validatePassaporte(ciclista, result);
+        }
+    }
+
+    private void validateCpf(Ciclista ciclista, BindingResult result) {
+        if (ciclista.getCpf() != null && !isValidCPF(ciclista.getCpf())) {
+            result.addError(new FieldError("ciclista", "cpf", "CPF inválido"));
+        }
+    }
+
+    private void validatePassaporte(Ciclista ciclista, BindingResult result) {
+        if (ciclista.getPassaporte() != null) {
+            if (ciclista.getPassaporte().getNumero() == null || ciclista.getPassaporte().getNumero().isEmpty()) {
+                result.addError(new FieldError("ciclista", "passaporte.numero", "Número do passaporte não pode ser nulo ou vazio"));
+            }
+            if (ciclista.getPassaporte().getValidade() == null || ciclista.getPassaporte().getValidade().isEmpty()) {
+                result.addError(new FieldError("ciclista", "passaporte.validade", "Validade do passaporte não pode ser nula ou vazia"));
+            }
+            if (ciclista.getPassaporte().getPais() == null || ciclista.getPassaporte().getPais().isEmpty()) {
+                result.addError(new FieldError("ciclista", "passaporte.pais", "País do passaporte não pode ser nulo ou vazio"));
+            }
         }
     }
 
