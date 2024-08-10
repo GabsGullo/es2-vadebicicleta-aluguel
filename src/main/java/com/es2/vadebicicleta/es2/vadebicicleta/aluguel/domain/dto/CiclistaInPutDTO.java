ec38c6aa7b38c5417af95c3bda09b94ea58bfa3d
GabsGullo <gabrielgullo30@gmail.com>
2024-08-10 00:36:44 -0300

    fixing issues

--- src/main/java/com/es2/vadebicicleta/es2/vadebicicleta/aluguel/domain/dto/CiclistaInPutDTO.java
+++ src/main/java/com/es2/vadebicicleta/es2/vadebicicleta/aluguel/domain/dto/CiclistaInPutDTO.java
@@ -3,10 +3,15 @@ package com.es2.vadebicicleta.es2.vadebicicleta.aluguel.domain.dto;
 import com.es2.vadebicicleta.es2.vadebicicleta.aluguel.domain.Ciclista;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
 
-@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CiclistaInPutDTO {
     @NotBlank
     private String nome;
@@ -20,4 +25,32 @@ public class CiclistaInPutDTO {
     @Email
     private String email;
     private String urlFotoDocumento;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setPassaporte(Ciclista.Passaporte passaporte) {
+        this.passaporte = passaporte;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUrlFotoDocumento(String urlFotoDocumento) {
+        this.urlFotoDocumento = urlFotoDocumento;
+    }
 }
